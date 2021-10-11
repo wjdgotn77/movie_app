8d2fd56aaa8f912cefe7ecf66fc8c276983c6e04
haesoo <wjdgotn77@gmail.com>
2021-10-11 22:29:07 +0900

    Lesson #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-// class component
-// 함수가 아니기때문에 return 을 가지지 않고 render method를 가진다.
-// ** react는 자동적으로 class component의 render method를 실행한다. **
+// componentDidMount() 에서 data를 fetch하고,
+// fetch가 완료되면 render()를 통해 화면에 그린다.
+// state는 동적으로 받아올 정보다. this.setState로 상태를 변경해줄 수 있다.
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
   state = {
-    // 동적 데이터를 담는 state.
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
-  }
+
   componentDidMount() {
-    console.log("component did mount");
+    // data fetch 하는 부분... 완료 되면 render 실행.
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 5000);
   }
-  componentDidUpdate() {
-    console.log("update")
-  }
+
   render() {
-    return (
-    <div>
-      <h1>The number is : {this.state.count}</h1>
-      <button type="button" onClick={this.add}>Add</button>
-      <button type="button" onClick={this.minus}>Minus</button>
-    </div>
-    );
+    // ES6 Destructoring assignment.
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready!"}</div>;
   }
 }
 
