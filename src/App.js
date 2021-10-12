d50cbe6d6c3723d4779ca52de545bfecb1ef5335
haesoo <wjdgotn77@gmail.com>
2021-10-12 21:33:09 +0900

    Lesson #4.1 Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
-// componentDidMount() 에서 data를 fetch하고,
-// fetch가 완료되면 render()를 통해 화면에 그린다.
-// state는 동적으로 받아올 정보다. this.setState로 상태를 변경해줄 수 있다.
+/* componentDidMount() 에서 data를 fetch하고,
+ * fetch가 완료되면 render()를 통해 화면에 그린다.
+ * state는 동적으로 받아올 정보다. this.setState로 상태를 변경해줄 수 있다.
+ */
 class App extends React.Component {
   state = {
     isLoading: true,
-    movies: []
+    movies: [],
   };
 
   getMovies = async () => {
-    // axios의 로딩이 끝나면 그 때 get을 실행해줘.
-    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    /* axios의 로딩이 끝나면 그 때 get을 실행해줘.
+     * const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+     * console.log(movies.data.data.movies);
+     */
+    // ES6 문법으로 위의 코드 작성.
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
@@ -21,8 +34,23 @@ class App extends React.Component {
 
   render() {
     // ES6 Destructoring assignment.
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready!"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
