3bd20f1970405d282cfcce6f7c08d55a71a074fb
haesoo <wjdgotn77@gmail.com>
2021-10-13 22:35:44 +0900

    Lesson #4.1 Rendering the Movies - add genres

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 /* componentDidMount() 에서 data를 fetch하고,
  * fetch가 완료되면 render()를 통해 화면에 그린다.
@@ -37,13 +38,13 @@ class App extends React.Component {
     // ES6 Destructoring assignment.
     const { isLoading, movies } = this.state;
     return (
-      <section class="container">
+      <section className="container">
         {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">"Loading..."</span>
+          <div className="loader">
+            <span className="loader__text">"Loading..."</span>
           </div>
         ) : (
-          <div class="movies">
+          <div className="movies">
             {movies.map((movie) => (
               <Movie
                 key={movie.id}
@@ -52,6 +53,7 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
           </div>
--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 //function Component의 인자로 props말고 props의 특정값만을 가지고 올때는 curly braket으로 감싸기!
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
     <div class="movie">
       <img src={poster} alt={title} title={title} />
       <div class="movie__data">
         <h3 class="movie__title">{title}</h3>
         <h5 class="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
         <p class="movie__summary">{summary}</p>
       </div>
     </div>
@@ -21,6 +29,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
